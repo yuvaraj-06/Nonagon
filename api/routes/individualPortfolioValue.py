20d5ba230e5d75c0ff28ab57c0c5738e84df991f
ksajan <ksajan12@gmail.com>
2022-08-27 21:49:24 +0530

    updated route with new information to present

--- api/routes/individualPortfolioValue.py
+++ api/routes/individualPortfolioValue.py
@@ -9,10 +9,10 @@ from api.controllers.covalent import CovalentServiceController
 portfolio_router = APIRouter(prefix="/api/v1", tags=["billing_status"])
 
 
-@portfolio_router.get("/portfolio_value/{chainid}/{address}")
+@portfolio_router.get("/portfolio_value/{address}")
 async def get_portfolio_value(address: str):
     """
     Returns the low and high portfolio value over time for a given address.
     """
     jackass = CovalentServiceController(address)
-    return jackass.getLowHigh()
+    return [[x for x, y in jackass.getLowHigh()], [y for x, y in jackass.getLowHigh()]]
